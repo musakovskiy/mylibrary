9fe9f8998b22c0753c67c8f462e12792e24734f8
musakovskiy <magazynuniversam@gmail.com>
2023-10-27 16:45:32 +0300

    comm5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, LiteraryFormatListView, BookListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("literary-formats/", LiteraryFormatListView.as_view(), name="literary-format-list"),
+    path("books/", BookListView.as_view(), name="book-list")
 ]
 app_name = "catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from .models import Book, Author, LiteraryFormat
 from django.shortcuts import render
+from django.views import generic
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -15,3 +16,19 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "catalog/index.html", context=context)
 
 
+class LiteraryFormatListView(generic.ListView):
+    model = LiteraryFormat
+    template_name = 'catalog/literary_format_list.html'
+    context_object_name = 'literary_format_list'
+
+
+# def literary_format_list_view(request: HttpRequest) -> HttpResponse:
+# literary_format_list = LiteraryFormat.objects.all()
+# context = {
+# "literary_format_list": literary_format_list
+# }
+# return render(request, "catalog/literary_format_list.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
