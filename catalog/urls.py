4d2a0377744c71e7ce5a12a8e754f311b04cd520
musakovskiy <magazynuniversam@gmail.com>
2023-10-31 19:53:37 +0200

    comm6

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from catalog.views import index, LiteraryFormatListView, BookListView
+from catalog.views import index, LiteraryFormatListView, BookListView, AuthorListView, book_detail_view, test_session_view
 
 urlpatterns = [
     path("", index, name="index"),
     path("literary-formats/", LiteraryFormatListView.as_view(), name="literary-format-list"),
-    path("books/", BookListView.as_view(), name="book-list")
+    path("books/", BookListView.as_view(), name="book-list"),
+    path("authors/", AuthorListView.as_view(), name="author-list"),
+    path("books/<int:pk>/", book_detail_view, name="book-detail"),
+    path('test-session/', test_session_view, name="test-session")
 ]
 app_name = "catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -8,10 +8,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_books = Book.objects.count()
     num_authors = Author.objects.count()
     num_literary_formats = LiteraryFormat.objects.count()
+    num_vizits = request.session.get("num_vizits", 0)
+    request.session["num_vizits"] = num_vizits + 1
     context = {
         "num_books": num_books,
         "num_authors": num_authors,
-        "num_literary_formats": num_literary_formats
+        "num_literary_formats": num_literary_formats,
+        "num_vizits": num_vizits + 1
     }
     return render(request, "catalog/index.html", context=context)
 
@@ -32,3 +35,28 @@ class LiteraryFormatListView(generic.ListView):
 
 class BookListView(generic.ListView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+def book_detail_view(request, pk: int) -> HttpResponse:
+    book = Book.objects.get(id=pk)
+    context = {
+        'book': book
+    }
+    return render(request, "catalog/book_detail.html", context=context)
+
+
+def test_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["book"] = "test session book"
+    request.session["format"] = "test format"
+    num_vizit = request.session.get("new", 0)
+    request.session["new"] = num_vizit + 1
+    return HttpResponse(
+        "<h1>Test Session</h1>"
+        f"<h4>Session Data: {request.session['book']}</4>"
+        f"<h3>New Session Data: {request.session['format']}"
+        f" <h4>You vizit this site : { num_vizit } times </h4>"
+    )
